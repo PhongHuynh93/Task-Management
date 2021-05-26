5fd47d1b973e478e4563f2712a8bb7c364d5c26c
Phong Huynh <hdthanhphong93@gmail.com>
2021-05-26 15:46:29 +0700

    Task and User entity, create relationship onetoman

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,5 +1,7 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm"
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm"
 import * as bcrypt from 'bcrypt'
+import { type } from "os";
+import { Task } from "src/tasks/task.entity";
 
 @Entity()
 @Unique(['username'])
@@ -15,6 +17,9 @@ export class User extends BaseEntity {
 
     @Column()
     salt: string
+
+    @OneToMany(type => Task, task => task.user, { eager: true })
+    tasks: Task[]
 
     async validatePassword(password: string) {
         const hash = await bcrypt.hash(password, this.salt)
