68a1f86f51b1d5bc6725e38e671ac1f28f957b12
Phong Huynh <hdthanhphong93@gmail.com>
2021-05-26 12:15:48 +0700

    Create access token and return it

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) { }
 
     async signUp(authCredentialsDto: AuthCredentialsDto) {
@@ -19,5 +22,9 @@ export class AuthService {
         if (!username) {
             throw new UnauthorizedException('Invalid credentials')
         }
+
+        const payload: JwtPayload = { username }
+        const accessToken = await this.jwtService.sign(payload)
+        return { accessToken }
     }
 }
