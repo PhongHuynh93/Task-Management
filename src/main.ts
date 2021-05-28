a53b4eb5c58fe4d20ffe664b17858bad49dc1ac7
Phong Huynh <hdthanhphong93@gmail.com>
2021-05-28 15:21:11 +0700

    Add config package, replace port with config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
-  const port = 3000
+
+  const config = require('config')
+  const serverConfig = config.get('server')
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application logging on port ${port}`)
 }
