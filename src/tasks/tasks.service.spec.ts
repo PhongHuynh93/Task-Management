7dbbba051db77875ef3985a71ad095387f4ff7c2
Phong Huynh <hdthanhphong93@gmail.com>
2021-06-02 21:39:51 +0700

    test service get tasks

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,10 +1,15 @@
 import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
+import { GetTasksFilterDto } from 'dist/tasks/dto/get-tasks-filter.dto';
+import { TaskStatus } from 'dist/tasks/task-status.enum';
  
 const mockTaskRepository = () => ({
     getTasks: jest.fn()
 })
+
+const mockUser = { id: 12, username: 'Test user' };
+
 describe('TaskService', () => {
     let tasksService
     let taskRepository
@@ -22,8 +27,14 @@ describe('TaskService', () => {
     })
 
     describe('getTasks', () => {
-        test('get all tasks from repository', () => {
+        test('get all tasks from repository', async () => {
+            taskRepository.getTasks.mockResolvedValue('someValue');
+
             expect(taskRepository.getTasks).not.toHaveBeenCalled()
+            const filters: GetTasksFilterDto = { status: TaskStatus.IN_PROGRESS, search: 'Some search query' };
+            const result = await tasksService.getTasks(filters, mockUser);
+            expect(taskRepository.getTasks).toHaveBeenCalled();
+            expect(result).toEqual('someValue');
         })
     })
 })
